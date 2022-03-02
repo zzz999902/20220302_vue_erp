944b9bf2ebd24d832a51f394cb8aa58ebc434ef6
肇旭阳 <z19x09y22@gmail.com>
2022-03-02 16:27:25 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import Antd from 'ant-design-vue';
 import App from './App.vue';
+import 'ant-design-vue/dist/antd.css';
 import router from './router';
 import store from './store';
+import '@/assets/css/reset.less';
 
 Vue.config.productionTip = false;
-
+Vue.use(Antd);
 new Vue({
   router,
   store,
